50c2ecb300d8fc2ccace8861c3a33bfdae6c883b
pedro <Pedrotaroco1@gmail.com>
2025-12-02 21:22:44 -0300

    Adicionando rota de horaatual e status

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.get('/status', (req, res) =>{
+    res.send("serviço operacional")
+})
+ 
+app.get('/horaatual', (req, res)=>{
+    res.send("22:21 hora de ir embora")
+})
 
 app.listen(3000)
